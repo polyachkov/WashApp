67000d8b7e1cad3592c1d723e69e1d227360ced6
Dmitry Polyachkov <d.polyachkov@g.nsu.ru>
2025-10-23 22:30:24 +0700

    SCRUM-12 init Spring Project with java 21

---
+++ Spring-back/src/test/java/ru/nsu/washapp/WashappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.washapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
